3ebda7948cc87778f9c540f3e3e43e03df3adce0
Denys <denysgdmgroup@gmail.com>
2022-08-12 14:14:38 +0400

    fixed transition on enter modal window

--- pages/profile/another-page/user-cabinet/my-profile.vue
+++ pages/profile/another-page/user-cabinet/my-profile.vue
@@ -226,15 +226,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.6s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
 .title-block {
   display: flex;
   justify-content: space-between;
