22769191fc5658c30138096958a89b320b5f9285
Denys <denysgdmgroup@gmail.com>
2022-09-06 16:15:08 +0400

    finished page of event

--- i18n/ua/index.js
+++ i18n/ua/index.js
@@ -2,10 +2,12 @@ import EventsPage from './events'
 import ProfilePage from './profile'
 import Modals from './modals'
 import Buttons from './buttons'
+import MyEvents from './my_events'
 
 export default {
   events: EventsPage,
   profile: ProfilePage,
   modals: Modals,
-  buttons: Buttons
+  buttons: Buttons,
+  my_events: MyEvents
 }
---
+++ i18n/ua/my_events.js
@@ -0,0 +1,6 @@
+export default {
+  'title': 'Дружній матч',
+  'subtitle': 'Оновіть своє фото та персональні дані',
+  'description-title': 'Опис події',
+  'already-accepted': 'Вже підтвердили участь'
+}
