943f000629c8c428fc126ee713aa1a160b80847b
Denys <denysgdmgroup@gmail.com>
2022-11-21 14:00:54 +0400

    created first endpoint

---
+++ src/workers/api-worker/http/http-services/versions.service.js
@@ -0,0 +1,13 @@
+import { AxiosInstance } from "../../../../plugins/axios.plugin";
+import { EndpointsEnum } from "../http-common/prefix.enum";
+
+export class VersionsService {
+  static getAllVersions() {
+    // return AxiosInstance.get(
+    //   EndpointsEnum.Users.getMyProfile
+    // )
+    return new Promise().resolve({
+      
+    })
+  }
+}
