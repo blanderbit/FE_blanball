33da65541c6574cb5b93b470fb5c9242f450b633
Djoncarton1921 <shapranov.nik@gmail.com>
2023-02-11 13:58:25 +0200

    BLAN-827 - fixed but with bugreport and create event review modals

--- src/stores/userData.js
+++ src/stores/userData.js
@@ -10,6 +10,7 @@ export const useUserDataStore = defineStore('userData', {
       return `${state.user.profile.name} ${state.user.profile.last_name}`
     },
     getUserPhone: (state) => state.user.phone,
+    getUserAvatar: (state) => state.user.profile.avatar_url,
   },
   actions: {
     // increment() {
