5b477a1df93e20ee9c241bfafaac0eab528c1d52
Djoncarton1921 <shapranov.nik@gmail.com>
2023-02-12 17:38:01 +0200

    BLAN-880 - translated messages of toasts into Ukrainian - changed text breadcrubms

--- src/i18n/ua/breadcrumbs.js
+++ src/i18n/ua/breadcrumbs.js
@@ -3,24 +3,24 @@ export default {
   profile: [{ name: 'Головна', path: '/' }, { name: 'Профіль' }],
   myEvents: [
     { name: 'Головна', path: '/' },
-    { name: 'Івенти', path: '/application/events' },
-    { name: 'Мої івенти' },
+    { name: 'Події', path: '/application/events' },
+    { name: 'Мої події' },
   ],
-  events: [{ name: 'Головна', path: '/' }, { name: 'Івенти' }],
+  events: [{ name: 'Головна', path: '/' }, { name: 'Події' }],
   createOneEvent: [
     {name: 'Головна', path: '/'},
-    {name: 'Івент', path: '/application/events'},
-    {name: 'Створення івенту '},
+    {name: 'Подія', path: '/application/events'},
+    {name: 'Створення події '},
   ],
   getOneEvent: [
     {name: 'Головна', path: '/'},
-    {name: 'Івенти', path: '/application/events'},
-    {name: 'Івент '},
+    {name: 'Події', path: '/application/events'},
+    {name: 'Подія'},
   ],
   editOneEvent:  [
     {name: 'Головна', path: '/'},
-    {name: 'Івенти', path: '/application/events'},
-    {name: 'Редагування івента'},
+    {name: 'Події', path: '/application/events'},
+    {name: 'Редагування події'},
   ],
   users: [
     {name: 'Головна', path: '/'},
