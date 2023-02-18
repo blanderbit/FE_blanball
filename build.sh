19c7333369a2f81c0c388204e5be75009b710222
Djoncarton1921 <shapranov.nik@gmail.com>
2023-02-18 21:33:40 +0200

    hotfix: changed build script

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ if [[ -d dist ]]
 then
     mv dist completed_build
 fi
-if [[ -d completed_build ]]
-then
-    cp deploy/.htaccess completed_build/.htaccess
-    cp -a completed_build/* /usr/share/nginx/html;
-    nginx -g 'daemon off;'
-if
+
+cp deploy/.htaccess completed_build/.htaccess
+cp -a completed_build/* /usr/share/nginx/html;
+nginx -g 'daemon off;'
