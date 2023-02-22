56c7b0b7d25b8c9f7a4647b6fce793bcda1d8154
Djoncarton1921 <shapranov.nik@gmail.com>
2023-02-22 10:23:17 +0200

    update buld.sh script

--- build.sh
+++ build.sh
@@ -8,10 +8,6 @@ npm run build
 
 # Check if build was successful
 if [[ -d /usr/share/nginx/html && -n $(ls /usr/share/nginx/html) ]]; then
-    # Create the /run directory and set the correct permissions
-    mkdir -p /run/nginx
-    chown -R nginx:nginx /run/nginx
-    chmod -R 755 /run/nginx
 
     # Start Nginx in the foreground
     nginx -g 'daemon off;'
@@ -19,11 +15,6 @@ else
     # Roll back to the previous backup
     latest_backup=$(ls -td /backups/html_* | head -n 1)
     cp -r "$latest_backup" /usr/share/nginx/html
-
-    # Create the /run directory and set the correct permissions
-    mkdir -p /run/nginx
-    chown -R nginx:nginx /run/nginx
-    chmod -R 755 /run/nginx
 
     # Start Nginx in the foreground
     nginx -g 'daemon off;'
