b9d0e83261f82cfb4196c6407b00be6487420f0e
Djoncarton1921 <shapranov.nik@gmail.com>
2023-02-22 10:15:03 +0200

    -updated build.sh script

--- build.sh
+++ build.sh
@@ -8,12 +8,23 @@ npm run build
 
 # Check if build was successful
 if [[ -d /usr/share/nginx/html && -n $(ls /usr/share/nginx/html) ]]; then
-    nginx -s reload
+    # Create the /run directory and set the correct permissions
+    mkdir -p /run/nginx
+    chown -R nginx:nginx /run/nginx
+    chmod -R 755 /run/nginx
+
+    # Start Nginx in the foreground
+    nginx -g 'daemon off;'
 else
     # Roll back to the previous backup
     latest_backup=$(ls -td /backups/html_* | head -n 1)
     cp -r "$latest_backup" /usr/share/nginx/html
 
-    # Reload Nginx
-    nginx -s reload
+    # Create the /run directory and set the correct permissions
+    mkdir -p /run/nginx
+    chown -R nginx:nginx /run/nginx
+    chmod -R 755 /run/nginx
+
+    # Start Nginx in the foreground
+    nginx -g 'daemon off;'
 fi
