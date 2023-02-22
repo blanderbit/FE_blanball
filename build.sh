29bd33fbe8a99c16686499a0c0ad12d75fa007a4
Djoncarton1921 <shapranov.nik@gmail.com>
2023-02-22 10:08:44 +0200

    -hotfix: fixed build project on the server

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
-npm install;
-npm run build;
+# Create a backup of the current build
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+cp -r /usr/share/nginx/html "/backups/html_$timestamp"
 
-if [[ -d dist ]]
-then
-    mv dist completed_build
+# Install dependencies and build the project
+npm install
+npm run build
+
+# Check if build was successful
+if [[ -d /usr/share/nginx/html && -n $(ls /usr/share/nginx/html) ]]; then
+    nginx -s reload
+else
+    # Roll back to the previous backup
+    latest_backup=$(ls -td /backups/html_* | head -n 1)
+    cp -r "$latest_backup" /usr/share/nginx/html
+
+    # Reload Nginx
+    nginx -s reload
 fi
-
-cp deploy/.htaccess completed_build/.htaccess
-cp -a completed_build/* /usr/share/nginx/html;
-nginx -g 'daemon off;'
