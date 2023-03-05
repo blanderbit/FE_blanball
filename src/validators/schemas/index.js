8a9816ce60c0079eaee105b59e36b7a3c6b28682
Djoncarton1921 <shapranov.nik@gmail.com>
2023-03-05 18:58:20 +0200

    BLAN-989 - password reset validation scheme moved to a separate file

--- src/validators/schemas/index.js
+++ src/validators/schemas/index.js
@@ -2,10 +2,12 @@ import Login from './login'
 import Register from './register'
 import Profile from './profile'
 import ManageEvent from './manageEvent'
+import Stepper from './stepper'
 
 export default {
     login: Login,
     register: Register,
     profile: Profile,
     manageEvent: ManageEvent,
+    stepper: Stepper
 }
